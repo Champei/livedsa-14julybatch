c570b45aedeb4d57cb715525d80729c85d302248
Kartik <kartik.mathur10@gmail.com>
2025-12-26 21:09:10 +0530

    Tries

--- L47-Tries/1_Tries.cpp
+++ L47-Tries/1_Tries.cpp
@@ -58,11 +58,40 @@ public:
 
 		return temp->isEnd; // This must be true yaha pata chlega ki word end hota hai ya nahi hota
 	}
+
+	// Recursion
+	void autoFillHelper(node* temp, string word) {
+		if (temp->isEnd == true) cout << word << endl;
+
+		for (auto p : temp->h) {
+			autoFillHelper(p.second, word + p.first);
+		}
+	}
+
+	void autoFill(string word) {
+		node* temp = root;
+
+		for (int i = 0; i < word.size(); ++i)
+		{
+			char ch = word[i];
+
+			if (temp->h.count(ch) == 1) {
+				temp = temp->h[ch];
+			}
+			else {
+				return;
+			}
+		}
+
+		autoFillHelper(temp, word);
+
+	}
 };
 
 int main() {
 
 	Trie t;
+	t.addWord("He");
 	t.addWord("Hello");
 	t.addWord("Her");
 	t.addWord("Hey");
@@ -70,8 +99,10 @@ int main() {
 	t.addWord("Coder");
 
 	string key = "Code";
-	if (t.searchWord(key)) cout << "Word Found\n";
-	else cout << "Word Not Found\n";
+	// if (t.searchWord(key)) cout << "Word Found\n";
+	// else cout << "Word Not Found\n";
+
+	t.autoFill("He");
 
 	return 0;
 }
